503f8ab9bd09dd7633440dca18f5dd7d0e7039bd
Vadym <93435125+vadymzag@users.noreply.github.com>
2025-08-15 22:16:17 +0300

    Add 11 lesson with mini project

---
+++ 11. SQLite/sql_code.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+# Create connection to db
+connection = sqlite3.connect("my_database.db")
+
+cursor = connection.cursor()
+
+query_create_table = """
+create table if not exists students (
+    id integer primary key,
+    name text not null,
+    age integer,
+    grade text
+);
+"""
+
+cursor.execute(query_create_table)
+connection.commit()
+
+name = "Alice"
+age = 18
+grade = "A"
+
+# cursor.execute("insert into students (name, age, grade) values (?, ?, ?)", (name, age, grade))
+# connection.commit()
+
+# student_data = [
+#     ("Bob", 20, "C"),
+#     ("Charlie", 19, "B"),
+#     ("Diana", 20, "A")
+# ]
+#
+# cursor.executemany("insert into students (name, age, grade) values (?, ?, ?)", student_data)
+# connection.commit()
+
+
+# drop_id = [(2,), (3,)]
+#
+# cursor.executemany("delete from students where id=(?)", drop_id)
+# connection.commit()
+
+query = """
+select *
+from students;
+"""
+
+# cursor.execute(query)
+# all_students = cursor.fetchall()
+# print(all_students)
+
+# age more than 19
+
+# cursor.execute("""select * from students where age > 19;""")
+# age_more_19 = cursor.fetchall()
+# print(age_more_19)
+
+# all bob students
+# cursor.execute("""select * from students where name=(?); """, ("Bob",))
+# bob_data = cursor.fetchall()
+# print(bob_data)
+
+# student_name = "Bob"
+# new_grade = "B"
+#
+# cursor.execute("Update students set grade = ? where name = ?", (new_grade, student_name))
+# connection.commit()
+
+
+# All students that have age < 20, will increase their age by 1.
+cursor.execute("Select * from students;")
+print(f"Before update: {cursor.fetchall()}")
+
+# Select all students with age < 20.
+cursor.execute("Select id, age from students where age < 20;")
+required_students = cursor.fetchall()
+
+for id_, age in required_students:
+    new_age = age + 1
+    cursor.execute("Update students set age = ? where id = ?;", (new_age, id_))
+
+connection.commit()
+
+cursor.execute("Select * from students;")
+print(f"After update: {cursor.fetchall()}")
+
